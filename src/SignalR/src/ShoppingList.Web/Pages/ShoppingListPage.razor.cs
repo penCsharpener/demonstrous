78029548968a7d9eab9e1f66af9f94f101ed223c
penCsharpener <26190934+penCsharpener@users.noreply.github.com>
2023-03-19 13:37:17 +0100

    split razor class

--- src/SignalR/src/ShoppingList.Web/Pages/ShoppingListPage.razor.cs
+++ src/SignalR/src/ShoppingList.Web/Pages/ShoppingListPage.razor.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.SignalR.Client;
 using Microsoft.EntityFrameworkCore;
 using ShoppingList.Web.Data;
@@ -5,11 +6,18 @@ using ShoppingList.Web.Models;
 
 namespace ShoppingList.Web.Pages;
 
-public partial class ShoppingListPage
+public partial class ShoppingListPage : IAsyncDisposable
 {
     private HubConnection? hubConnection;
     public ShoppingItem NewItem { get; set; } = new();
     public List<ShoppingItem> ShoppingItems { get; set; } = new();
+
+    [Inject]
+    public IServiceScopeFactory ScopeFactory { get; set; } = default!;
+
+    [Inject]
+    public NavigationManager Navigation { get; set; } = default!;
+
     protected override async Task OnInitializedAsync()
     {
         using (var scope = ScopeFactory.CreateScope())
@@ -19,8 +27,11 @@ public partial class ShoppingListPage
         }
 
         var url = Navigation.ToAbsoluteUri("/shoppingaction");
-        Console.WriteLine(url);
-        hubConnection = new HubConnectionBuilder().WithUrl(url).WithAutomaticReconnect().ConfigureLogging(conf => conf.AddConsole()).Build();
+        hubConnection = new HubConnectionBuilder()
+            .WithUrl(url)
+            .WithAutomaticReconnect()
+            .ConfigureLogging(conf => conf.AddConsole()).Build();
+
         hubConnection.On<ShoppingItemNotification>("ItemListUpdated", item =>
         {
             if (item.WasDeleted && ShoppingItems.FirstOrDefault(s => s.Id == item.Id) is { } existingItem)
@@ -35,6 +46,7 @@ public partial class ShoppingListPage
 
             InvokeAsync(StateHasChanged);
         });
+
         await hubConnection.StartAsync();
     }
 
@@ -48,13 +60,17 @@ public partial class ShoppingListPage
         }
 
         using var scope = ScopeFactory.CreateScope();
+
         var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
         context.ShoppingItems.Add(NewItem);
+
         await context.SaveChangesAsync();
+
         var notification = new ShoppingItemNotification(NewItem)
         {
             WasAdded = true
         };
+
         if (hubConnection is not null)
         {
             await hubConnection.SendAsync("NotifyNewItem", notification);
@@ -67,19 +83,24 @@ public partial class ShoppingListPage
     private async Task HandleDeleteAsync(int id)
     {
         using var scope = ScopeFactory.CreateScope();
+
         var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
         var item = await context.ShoppingItems.FirstOrDefaultAsync(s => s.Id == id);
+
         var notification = new ShoppingItemNotification(item)
         {
             WasDeleted = true
         };
+
         context.ShoppingItems.Remove(item);
+
         if (hubConnection is not null)
         {
             await hubConnection.SendAsync("NotifyNewItem", notification);
         }
 
         ShoppingItems.Remove(ShoppingItems.First(s => s.Id == id));
+
         await context.SaveChangesAsync();
     }
 
