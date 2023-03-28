e9398835eef604fbe7521020b0b9201e9e2bba9b
penCsharpener <26190934+penCsharpener@users.noreply.github.com>
2023-03-28 23:03:36 +0200

    add serilog, use kestrel configuration in appsettings

--- src/RESTful/src/Restful.Api/Program.cs
+++ src/RESTful/src/Restful.Api/Program.cs
@@ -1,35 +1,33 @@
+using Serilog;
 
 namespace Restful.Api;
 
 public class Program
 {
-  public static void Main(string[] args)
-  {
-    var builder = WebApplication.CreateBuilder(args);
-
-    // Add services to the container.
-
-    builder.Services.AddControllers();
-    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-    builder.Services.AddEndpointsApiExplorer();
-    builder.Services.AddSwaggerGen();
-
-    var app = builder.Build();
-
-    // Configure the HTTP request pipeline.
-    if (app.Environment.IsDevelopment())
+    public static void Main(string[] args)
     {
-      app.UseSwagger();
-      app.UseSwaggerUI();
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Host.UseSerilog((hostingContext, loggerConfiguration) => loggerConfiguration.ReadFrom.Configuration(hostingContext.Configuration));
+
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
     }
-
-    app.UseHttpsRedirection();
-
-    app.UseAuthorization();
-
-
-    app.MapControllers();
-
-    app.Run();
-  }
 }
